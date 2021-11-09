fb020f4b6b54be95813d7ff6e3b15f4f4b0f53bd
tejeoner34 <atzarco37@gmail.com>
2021-11-09 09:41:09 +0100

    creación de carpetas de componentes, hooks y pages y cambio del logo de la web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import KanbanPage from './pages/kanban-page';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    {/* aqui va el header */}
+      <Switch>
+
+        <Route path="/kanban">
+
+          {/* aqui va la página del kanban */}
+          <KanbanPage></KanbanPage>
+
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/pages/kanban-page/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css'
+
+function KanbanPage(){
+
+    return(
+        <h1>pagina kanban</h1>
+    )
+}
+
+export default KanbanPage;
